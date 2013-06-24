8945e74eebeb222b239105a5582b67be242734d2
Tatyana Lomonosova <Tatjana.Lomonosova@noveogroup.com>
2013-06-24 16:02:02 +0700

    Forgotten tests

---
+++ MasonryNotForHuman.cpp
@@ -0,0 +1,183 @@
+
+#include "MasonryNotForHuman.h"
+#include <assert.h>
+
+void strangeAndUglyTests()
+{
+    
+    std::vector<DiscreteRect> someRects = std::vector<DiscreteRect> ();
+    
+    someRects.push_back(DiscreteRect(1, 3, 2, 4));
+    someRects.push_back(DiscreteRect(2, 5, 1, 1));
+    someRects.push_back(DiscreteRect(3, 7, 1, 5));
+    
+    std::vector<DiscreteRect> someLayoutedRects = std::vector<DiscreteRect> ();
+    
+    someLayoutedRects.push_back(DiscreteRect(0, 3, 2, 4));
+    someLayoutedRects.push_back(DiscreteRect(2, 5, 1, 1));
+    someLayoutedRects.push_back(DiscreteRect(3, 7, 1, 5));
+    
+    std::vector<DiscreteRect> somePhotos = std::vector<DiscreteRect> ();
+    
+    somePhotos.push_back(DiscreteRect(1, 1));
+    somePhotos.push_back(DiscreteRect(2, 1));
+    somePhotos.push_back(DiscreteRect(1, 5));
+    
+    std::vector<DiscreteRect> rectsForSomePhotos = std::vector<DiscreteRect> ();
+    
+    rectsForSomePhotos.push_back(DiscreteRect(0, 0, 2, 1));
+    rectsForSomePhotos.push_back(DiscreteRect(0, 1, 2, 1));
+    rectsForSomePhotos.push_back(DiscreteRect(0, 2, 1, 5));
+    
+    assert(!DiscreteRect(3, 0, 2, 0).containsColumn(1));
+    assert(!DiscreteRect(3, 0, 2, 0).containsColumn(2));
+    assert(DiscreteRect(3, 0, 2, 0).containsColumn(3));
+    assert(DiscreteRect(3, 0, 2, 0).containsColumn(4));
+    assert(!DiscreteRect(3, 0, 2, 0).containsColumn(5));
+    assert(!DiscreteRect(3, 0, 2, 0).containsColumn(6));
+    assert(!DiscreteRect(3, 0, 2, 0).containsColumn(7));
+
+    assert(8 == DiscreteRect(0, 3, 0, 5).bottom());
+
+    assert(0 == bottomInColumn(0, someRects));
+    assert(7 == bottomInColumn(1, someRects));
+    assert(7 == bottomInColumn(2, someRects));
+    assert(12 == bottomInColumn(3, someRects));
+    assert(0 == bottomInColumn(4, someRects));
+    assert(0 == bottomInColumn(5, someRects));
+    assert(0 == bottomInColumn(0, {}));
+
+    {
+        std::vector<unsigned> shape = lowerEdgeShape(4, someRects);
+        unsigned templateShape[] = {0, 7, 7, 12};
+        
+        assert(std::equal(shape.begin()
+                          , shape.end()
+                          , templateShape
+                          , [](unsigned const x, unsigned const y){ return x == y; }));
+    }
+
+    {
+        std::vector<unsigned> shape = lowerEdgeShape(4, someLayoutedRects);
+        unsigned templateShape[] = {7, 7, 6, 12};
+
+        assert(std::equal(shape.begin()
+                          , shape.end()
+                          , templateShape
+                          , [](unsigned const x, unsigned const y){ return x == y; }));
+    }
+
+    assert(7 == freeYAtColumnForWidth(0, 2, {0, 7, 7}));
+    assert(0 == freeYAtColumnForWidth(0, 1, {0, 7, 7}));
+
+    {
+        std::vector<DiscreteRect> rects = {
+            DiscreteRect(0, 0, 1, 2)
+            , DiscreteRect(1, 0, 1, 1)};
+        
+        std::vector<DiscreteRect> rectsEliminatedGaps = {
+            DiscreteRect(0, 0, 1, 2)
+            , DiscreteRect(1, 0, 1, 2)
+            , DiscreteRect(0, 2, 2, 1)};
+        
+        DiscreteRect gap = DiscreteRect(1, 1, 1, 1);
+        DiscreteRect rect = DiscreteRect(0, 2, 2, 1);
+        
+        eliminateGap(2, gap, rects, rect);
+        
+        assert(std::equal(rects.begin(), rects.end(), rectsEliminatedGaps.begin(), [](DiscreteRect const r1, DiscreteRect const r2){ return r1 == r2; }));
+    }
+    
+    {
+        std::vector<DiscreteRect> rects = {
+            DiscreteRect(0, 0, 1, 1)};
+        
+        std::vector<DiscreteRect> rectsEliminatedGaps = {
+            DiscreteRect(0, 0, 2, 1)
+            , DiscreteRect(0, 1, 2, 1)};
+        
+        DiscreteRect gap = DiscreteRect(1, 0, 1, 1);
+        DiscreteRect rect = DiscreteRect(0, 1, 2, 1);
+        
+        eliminateGap(2, gap, rects, rect);
+        
+        assert(std::equal(rects.begin(), rects.end(), rectsEliminatedGaps.begin(), [](DiscreteRect const r1, DiscreteRect const r2){ return r1 == r2; }));
+    }
+
+    assert(DiscreteRect(2, 0, 1, 1) == findFirstGap(DiscreteRect(1, 1, 2, 1), {1, 1, 0, 1}));
+    assert(DiscreteRect(0, 0, 1, 1) == findFirstGap(DiscreteRect(0, 1, 3, 1), {0, 1, 0, 1}));
+    assert(DiscreteRect(0, 0, 1, 1) == findFirstGap(DiscreteRect(0, 1, 2, 1), {0, 1, 1, 1}));
+    assert(DiscreteRect(1, 1, 1, 0) == findFirstGap(DiscreteRect(1, 1, 2, 1), {1, 1, 1, 1}));
+
+    {
+        std::vector<unsigned> freePlacesTest = {7, 7, 12};
+        std::vector<unsigned> freePlaces = freePoints(2, {0, 7, 7, 12});
+        assert(std::equal(freePlacesTest.begin(), freePlacesTest.end(), freePlaces.begin(), [](unsigned const x, unsigned const y){ return x == y; }));
+    }
+    
+    {
+        std::vector<unsigned> freePlacesTest = {1, 1, 1};
+        std::vector<unsigned> freePlaces = freePoints(2, {0, 1, 0, 1, 0});
+        assert(std::equal(freePlacesTest.begin(), freePlacesTest.end(), freePlaces.begin(), [](unsigned const x, unsigned const y){ return x == y; }));
+    }
+    
+    {
+        std::vector<unsigned> freePlacesTest = {0, 7, 7, 12};
+        std::vector<unsigned> freePlaces = freePoints(1, {0, 7, 7, 12});
+        assert(std::equal(freePlacesTest.begin(), freePlacesTest.end(), freePlaces.begin(), [](unsigned const x, unsigned const y){ return x == y; }));
+    }
+
+/*-------- Layout iteration ---------*/
+    {
+        std::vector<DiscreteRect> rects = {DiscreteRect(0, 0, 2, 42)};
+        std::vector<DiscreteRect> outRects = std::vector<DiscreteRect>();
+        
+        layoutIteration(2, outRects, DiscreteRect(2, 42));
+        assert(std::equal(rects.begin(), rects.end(), outRects.begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    {
+        std::vector<DiscreteRect> rects = {DiscreteRect(0, 0, 2, 42), DiscreteRect(0, 42, 2, 42)};
+        std::vector<DiscreteRect> outRects = {DiscreteRect(0, 0, 2, 42)};
+        
+        layoutIteration(2, outRects, DiscreteRect(2, 42));
+        assert(std::equal(rects.begin(), rects.end(), outRects.begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    {
+        std::vector<DiscreteRect> rects = {DiscreteRect(0, 0, 2, 42), DiscreteRect(2, 0, 2, 42)};
+        std::vector<DiscreteRect> outRects = {DiscreteRect(0, 0, 2, 42)};
+        
+        layoutIteration(4, outRects, DiscreteRect(2, 42));
+        assert(std::equal(rects.begin(), rects.end(), outRects.begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    {
+        std::vector<DiscreteRect> rects = std::vector<DiscreteRect> (someLayoutedRects);
+        rects.push_back(DiscreteRect(0, 7, 2, 42));
+        std::vector<DiscreteRect> outRects = std::vector<DiscreteRect> (someLayoutedRects);
+        
+        layoutIteration(4, outRects, DiscreteRect(2, 42));
+        assert(std::equal(rects.begin(), rects.end(), outRects.begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    {
+        std::vector<DiscreteRect> rects = std::vector<DiscreteRect> (someLayoutedRects);
+        rects.push_back(DiscreteRect(2, 6, 1, 42));
+        std::vector<DiscreteRect> outRects = std::vector<DiscreteRect> (someLayoutedRects);
+        
+        layoutIteration(4, outRects, DiscreteRect(1, 42));
+        assert(std::equal(rects.begin(), rects.end(), outRects.begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+
+
+/*------------ Layout ----------------*/
+    
+    {
+        std::vector<DiscreteRect> photos = {};
+        std::vector<DiscreteRect> rects = {};
+        assert(std::equal(rects.begin(), rects.end(), masonry(2, rects).begin(),  [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    {
+        std::vector<DiscreteRect> photos = {DiscreteRect(1, 1)};
+        std::vector<DiscreteRect> rects = {DiscreteRect(0, 0, 1, 1)};
+        assert(std::equal(rects.begin(), rects.end(), masonry(2, rects).begin(),  [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+    }
+    assert(std::equal(rectsForSomePhotos.begin(), rectsForSomePhotos.end(), masonry(2, somePhotos).begin(), [](DiscreteRect const &r1, DiscreteRect const &r2){ return r1 == r2; }));
+}
---
+++ MasonryNotForHuman.h
@@ -0,0 +1,46 @@
+//
+//  MasonryNotForHuman.h
+//  Masonry
+//
+
+#ifndef __Masonry__MasonryNotForHuman__
+#define __Masonry__MasonryNotForHuman__
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#include "DiscreteRect.h"
+
+std::vector<DiscreteRect> masonry(unsigned const gridWidth, std::vector<DiscreteRect> const &);
+
+void strangeAndUglyTests();
+unsigned bottomInColumn (unsigned const col
+                         , std::vector<DiscreteRect> const &rects);
+std::vector<unsigned> lowerEdgeShape (unsigned const gridWidth
+                                      , std::vector<DiscreteRect> const &rects);
+    
+unsigned freeYAtColumnForWidth (unsigned const col
+                                , unsigned const width
+                                , std::vector<unsigned> const &shape);
+std::vector<unsigned> freePoints (unsigned const width
+                                  , std::vector<unsigned> const &shape);
+DiscreteRect findFirstGap (DiscreteRect const &rect
+                           , std::vector<unsigned> const &shape);
+std::vector<DiscreteRect>::iterator findTopNeighbor (DiscreteRect const &gap
+                                                     , std::vector<DiscreteRect> &rects);
+std::vector<DiscreteRect>::iterator findLeftNeighbor (DiscreteRect const &gap
+                                                      , std::vector<DiscreteRect> &rects);
+void layoutIteration (unsigned const gridWidth, std::vector<DiscreteRect> &rects, DiscreteRect const &photo);
+
+void eliminateGap (unsigned const gridWidth
+                   , DiscreteRect const &gap
+                   , std::vector<DiscreteRect> &rects
+                   , DiscreteRect const &newRect);
+void refine (unsigned const gridWidth
+             , std::vector<DiscreteRect> &rects
+             , DiscreteRect const &newRect
+             , std::vector<unsigned> const &shape);
+std::vector<DiscreteRect> masonry(unsigned const gridWidth, std::vector<DiscreteRect> const &photos);
+
+#endif /* defined(__Masonry__MasonryNotForHuman__) */
